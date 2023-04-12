2f1ec05ad606960da744d2d166761fffa377a1aa
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-12 23:15:30 +0200

    changes to header

--- osc-hazard-vinyl-store/src/Components/Header/Header.js
+++ osc-hazard-vinyl-store/src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import { SearchBar } from "./SearchBar/SearchBar";
 
 export const Header = (props) => {
   return (
-    <header className="bg-white p-4 flex justify-evenly items-center border-red-500 border-2 h-40">
+    <header className="bg-white p-4 flex justify-evenly items-center h-40">
       <NavBar />
       <SearchBar />
     </header>
