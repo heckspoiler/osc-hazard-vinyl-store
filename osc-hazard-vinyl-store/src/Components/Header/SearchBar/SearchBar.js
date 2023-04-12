3e473e2a4d1495b1c0c9e382d0c5f96eb7a012b1
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-12 21:08:22 +0200

    adding Autosuggest to SearchBar

--- osc-hazard-vinyl-store/src/Components/Header/SearchBar/SearchBar.js
+++ osc-hazard-vinyl-store/src/Components/Header/SearchBar/SearchBar.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import searchIcon from "../../../icons/searchIcon.svg";
+import { Autosuggest, getSuggestions } from "react-autosuggest";
+import "../../../CSS/Autosuggest.css";
 
 export const SearchBar = () => {
   const [search, setSearch] = useState("");
@@ -12,11 +14,14 @@ export const SearchBar = () => {
     e.preventDefault();
   };
 
+  const renderSuggestion = (suggestion) => {
+    return <div>{suggestion.name}</div>;
+  };
+
   return (
     <form
       onSubmit={handleSearchSubmit}
-      className="searchbar-container absolute right-96 w-auto
-      h-14 flex items-center justify-center gap-2 mt-[-4rem]"
+      className="searchbar-container absolute right-96 w-auto h-14 flex items-center justify-center gap-2 mt-[-4rem]"
     >
       <button type="submit" className="h-5 w-5">
         <img src={searchIcon} alt="Search Icon" />
@@ -26,7 +31,7 @@ export const SearchBar = () => {
         placeholder="Search..."
         value={search}
         onChange={handleSearch}
-        className="pl-5 bg-white border-black border color-black font-mono h-6 w-48 focus:border-FFEE57 text-xs"
+        className="pl-5 bg-white border-black border text-black font-mono h-6 w-48 focus:border-FFEE57 text-xs"
       />
     </form>
   );
