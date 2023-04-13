3790da8f0e677dd59719ec2e2d1439b54677bcd8
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-13 21:28:30 +0200

    login section for now done, adding buttons

--- osc-hazard-vinyl-store/src/Components/Header/LoginSection/LoginSection.js
+++ osc-hazard-vinyl-store/src/Components/Header/LoginSection/LoginSection.js
@@ -2,14 +2,23 @@ import React, { useState } from "react";
 import defaultFace from "../../../icons/default-face.png";
 
 export const LoginSection = () => {
+  const [image, setImage] = useState();
   return (
-    <div className="LoginSection border border-black absolute right-10 h-16 w-16">
-      <div classList="imageContainer">
-        <img src={defaultFace} alt="Login Placeholder Image" />
+    <div className="LoginSection absolute right-40 h-32 w-32 flex flex-col justify-center items-center gap-4">
+      <div className="imageContainer h-16 w-16 border-black border flex items-center justify-center">
+        <img
+          className="h-12 w-12"
+          src={image || defaultFace}
+          alt="Login Placeholder Image"
+        />
       </div>
-      <div classList="ButtonContainer">
-        <button></button>
-        <button></button>
+      <div className="ButtonContainer flex gap-1 font-mono">
+        <button className="border border-black p-1 rounded-2xl w-16 text-xs">
+          Log in
+        </button>
+        <button className="border border-black p-1 rounded-2xl w-16 text-xs">
+          Sign up
+        </button>
       </div>
     </div>
   );
--- osc-hazard-vinyl-store/tailwind.config.js
+++ osc-hazard-vinyl-store/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./public/index.html",
     "./src/Components/Header/Header.js",
     "./src/Components/Header/SearchBar/SearchBar.js",
+    "./src/Components/Header/LoginSection/LoginSection.js",
   ],
   theme: {
     extend: {
