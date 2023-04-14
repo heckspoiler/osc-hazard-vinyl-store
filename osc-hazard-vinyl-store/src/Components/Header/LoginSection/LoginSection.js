eb2c2aeb4a7e1a24c03f68dc5abe4f5ae4539f68
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-14 20:16:48 +0200

    easier for gradient

--- osc-hazard-vinyl-store/src/Components/Header/LoginSection/LoginSection.js
+++ osc-hazard-vinyl-store/src/Components/Header/LoginSection/LoginSection.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { LoginButton } from "./LoginButton/LoginButton";
 import defaultFace from "../../../icons/default-face.png";
+import "./LoginSection.css";
 
 export const LoginSection = () => {
   const [image, setImage] = useState();
   return (
     <div className="LoginSection absolute right-40 h-32 w-32 flex flex-col justify-center items-center gap-4">
-      <div className="imageContainer h-16 w-16 border-black border flex items-center justify-center">
+      <div className="imageContainer h-16 w-16 border-black border flex items-center justify-center ">
         <img
           className="h-12 w-12"
           src={image || defaultFace}
