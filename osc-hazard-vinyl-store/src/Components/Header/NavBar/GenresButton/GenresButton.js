ced96bce0e4a64ab9699df8b1237b2c12fbd28cd
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-15 20:34:13 +0200

    Genres Button Dropdown

--- osc-hazard-vinyl-store/src/Components/Header/NavBar/GenresButton/GenresButton.js
+++ osc-hazard-vinyl-store/src/Components/Header/NavBar/GenresButton/GenresButton.js
@@ -4,11 +4,22 @@ import "./GenresButton.css";
 
 export const GenresButton = () => {
   const [showDropdown, setShowDropdown] = useState(false);
-  const handleGenresHover = () => {
+  const handleGenresHover = (event) => {
     setShowDropdown(!showDropdown);
+    const genresButtonAfter = event.target.querySelector(".genres-arrow");
+    genresButtonAfter.style.transform = "rotate(90deg)";
+    genresButtonAfter.style.transition = "transform 0.2s ease-in-out";
+  };
+  const handleGenresLeave = (event) => {
+    const genresButtonAfter = event.target.querySelector(".genres-arrow");
+    genresButtonAfter.style.transform = "none";
   };
   return (
-    <div>
+    <div
+      className="genres-button-container"
+      onMouseEnter={() => setShowDropdown(true)}
+      onMouseLeave={() => setShowDropdown(false)}
+    >
       <button className="list-item bg-white border-b border-black font-mono tracking-wider h-auto w-auto p-3 border-bottom-width transition-all duration-200 ease-in-out flex px-4 justify-center align-center text-xs hover:bg-violet-300">
         Genres
       </button>
--- osc-hazard-vinyl-store/src/Components/Header/NavBar/NavBar.js
+++ osc-hazard-vinyl-store/src/Components/Header/NavBar/NavBar.js
@@ -1,21 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { GenresButton } from "./GenresButton/GenresButton";
-import DropdownArrow from "../../../icons/caret-down-thin.svg";
 import "./NavBar.css";
 
 export const NavBar = (props) => {
-  const handleGenresHover = (event) => {
-    const genresButtonAfter = event.target.querySelector(".genres-arrow");
-    genresButtonAfter.style.transform = "rotate(90deg)";
-    genresButtonAfter.style.transition = "transform 0.2s ease-in-out";
-  };
-
-  const handleGenresLeave = (event) => {
-    const genresButtonAfter = event.target.querySelector(".genres-arrow");
-    genresButtonAfter.style.transform = "none";
-  };
-
   return (
     <ul className="flex justify-between text-black p-4 w-3/5 gap-5 absolute mt-24">
       <li className="list-item bg-white border-b border-black font-mono tracking-wider h-auto w-auto p-3 border-bottom-width transition-all duration-200 ease-in-out flex px-4 justify-center align-center text-xs hover:bg-violet-300">
@@ -29,8 +17,8 @@ export const NavBar = (props) => {
       </li>
       <li
         className="list-item bg-white border-b border-black font-mono tracking-wider h-auto w-auto p-3 border-bottom-width transition-all duration-200 ease-in-out flex px-4 justify-center align-center text-xs hover:bg-violet-300"
-        onMouseEnter={handleGenresHover}
-        onMouseLeave={handleGenresLeave}
+        // onMouseEnter={handleGenresHover}
+        // onMouseLeave={handleGenresLeave}
       >
         <Link to="/genres" className="genres">
           Genres
