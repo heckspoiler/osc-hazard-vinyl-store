f4a660c8b84715b27649d5209868e899ea053e6a
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-15 22:00:38 +0200

    adding body

--- osc-hazard-vinyl-store/src/Components/Header/Header.js
+++ osc-hazard-vinyl-store/src/Components/Header/Header.js
@@ -9,19 +9,19 @@ export const Header = (props) => {
   return (
     <header className="bg-white p-4 flex justify-evenly items-center h-40">
       <div className="header-text-container flex gap-5 text-xxs text-gray-400 font-mono">
-        <p className="flex">
+        <p className="flex width-32 gap-2">
           <img src={Decor} className="decor-svg" />
           Worldwide Shipping
         </p>
-        <p className="flex">
+        <p className="flex width-20 gap-2">
           <img src={Decor} className="decor-svg" />
           Discount on First Purchase
         </p>
-        <p className="flex">
+        <p className="flex width-20 gap-2">
           <img src={Decor} className="decor-svg" />
           Curbside Pickup available
         </p>
-        <p className="flex">
+        <p className="flex width-20 gap-2">
           <img src={Decor} className="decor-svg" />
           New Collections Every Week
         </p>
