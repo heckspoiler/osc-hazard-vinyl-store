142d4b80860d03d63604f2429faf1a95132cc396
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-04-16 15:40:34 +0200

    imageSlider set up

--- osc-hazard-vinyl-store/src/Components/Body/ImageSlider/ImageSlider.js
+++ osc-hazard-vinyl-store/src/Components/Body/ImageSlider/ImageSlider.js
@@ -1,14 +1,30 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import { Slide } from "./Slide/Slide";
+import React, { useState } from "react";
+import { Slide, slideData } from "./Slide/Slide"; // Import slideData from Slide component
 import "./ImageSlider.css";
+import Arrow from "./arrow.svg";
 
 export const ImageSlider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % 3);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? 2 : prevSlide - 1));
+  };
+
   return (
-    <section className="image-slider">
-      <section className="image-slider__container">
-        <Slide />
-      </section>
-    </section>
+    <div className="content-slider relative">
+      <Slide slide={slideData[currentSlide]} />
+      <div className="arrow-container">
+        <div className="arrow arrow-left" onClick={prevSlide}>
+          <img src={Arrow} />
+        </div>
+        <div className="arrow arrow-right" onClick={nextSlide}>
+          <img src={Arrow} />
+        </div>
+      </div>
+    </div>
   );
 };
--- osc-hazard-vinyl-store/src/Components/Body/ImageSlider/Slide/Slide.js
+++ osc-hazard-vinyl-store/src/Components/Body/ImageSlider/Slide/Slide.js
@@ -3,57 +3,51 @@ import "./Slide.css";
 import Image1 from "./images/cactus.jpg";
 import Cover1 from "./images/cover_1.jpg";
 
-export const Slide = ({ text, smallImages, largeImages }) => {
-  const slideData = [
-    {
-      artistName: "Velvet Dynamite Exorcism",
-      releaseName: "Four Hours Too Long EP",
-      text: "Velvet Dynamite Exorcism's latest opus, <i>Four Hours Too Long<i>, is a triumphant exploration of experimental niche music. Renowned for their abstract structures, the band has transcended boundaries with an immersive auditory experience that challenges the listener's perceptions. Complex arrangements intertwine with dissonant harmonies, creating a meticulously crafted sonic landscape that dares to defy convention. This album solidifies Velvet Dynamite Exorcism's position as avant-garde maestros, fearlessly pushing the envelope of what music can be, and inviting the audience to delve into the depths of their groundbreaking vision.",
-      smallImages: Cover1,
-      largeImage: Image1,
-    },
-    {
-      artistName: "",
-      releaseName: "",
-      text: "Slide 2 Text",
-      smallImages: ["img2a.jpg", "img2b.jpg", "img2c.jpg", "img2d.jpg"],
-      largeImage: "largeImg2.jpg",
-    },
-    {
-      artistName: "",
-      releaseName: "",
-      text: "Slide 3 Text",
-      smallImages: ["img3a.jpg", "img3b.jpg", "img3c.jpg", "img3d.jpg"],
-      largeImage: "largeImg3.jpg",
-    },
-  ];
+export const slideData = [
+  {
+    artistName: "Velvet Dynamite Exorcism",
+    releaseName: "Four Hours Too Long EP",
+    text: "Velvet Dynamite Exorcism's latest opus, \"Four Hours Too Long\", is a triumphant exploration of experimental niche music. Renowned for their abstract structures, the band has transcended boundaries with an immersive auditory experience that challenges the listener's perceptions. Complex arrangements intertwine with dissonant harmonies, creating a meticulously crafted sonic landscape that dares to defy convention. This album solidifies Velvet Dynamite Exorcism's position as avant-garde maestros, fearlessly pushing the envelope of what music can be, and inviting the audience to delve into the depths of their groundbreaking vision.",
+    smallImages: [Cover1, Cover1, Cover1, Cover1],
+    largeImage: Image1,
+  },
+  {
+    artistName: "",
+    releaseName: "",
+    text: "Slide 2 Text",
+    smallImages: ["img2a.jpg", "img2b.jpg", "img2c.jpg", "img2d.jpg"],
+    largeImage: "largeImg2.jpg",
+  },
+  {
+    artistName: "",
+    releaseName: "",
+    text: "Slide 3 Text",
+    smallImages: ["img3a.jpg", "img3b.jpg", "img3c.jpg", "img3d.jpg"],
+    largeImage: "largeImg3.jpg",
+  },
+];
+
+export const Slide = ({ slide }) => {
   return (
     <section className="slide grid grid-cols-3">
       <section className="slider-text h-80">
         <section className="artist-name font-mono text-xl">
-          {slideData[0].artistName}
+          {slide.artistName}
         </section>
         <section className="release-name font-mono text-l">
-          {slideData[0].releaseName}
+          {slide.releaseName}
         </section>
-        <section className="text">{slideData[0].text}</section>
+        <section className="text">{slide.text}</section>
       </section>
       <section className="slider-vinyl-cover grid grid-cols-2 gap-y-0 h-80">
-        <div className="vinyl-cover">
-          <img className="vinyl-cover-img" src={slideData[0].smallImages} />
-        </div>
-        <div className="vinyl-cover">
-          <img className="vinyl-cover-img" src={slideData[0].smallImages} />
-        </div>
-        <div className="vinyl-cover">
-          <img className="vinyl-cover-img" src={slideData[0].smallImages} />
-        </div>
-        <div className="vinyl-cover">
-          <img className="vinyl-cover-img" src={slideData[0].smallImages} />
-        </div>
+        {slide.smallImages.map((image, index) => (
+          <div key={index} className="vinyl-cover">
+            <img className="vinyl-cover-img" src={image} />
+          </div>
+        ))}
       </section>
       <section className="slider-image">
-        <img src={slideData[0].largeImage} className="slider-image-img" />
+        <img src={slide.largeImage} className="slider-image-img" />
       </section>
     </section>
   );
